602eae40d68bd059c63e781e89e7fa6203053e05
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2023-07-25 22:31:10 +0200

    Deps: update remaining dependencies,

--- src/__example-files__/uses-fetch.preval.ts
+++ src/__example-files__/uses-fetch.preval.ts
@@ -1,10 +1,12 @@
 import preval from 'next-plugin-preval';
 
 async function getDataViaFetch() {
-  const response = await fetch('https://example.com');
-  const html = await response.text();
+  const response = await fetch(
+    'https://api.github.com/repos/sweet-side-of-sweden/next-plugin-preval/readme'
+  );
+  const result = await response.json();
 
-  return { html };
+  return result.name;
 }
 
 export default preval(getDataViaFetch());
